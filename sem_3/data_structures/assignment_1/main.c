5f089f148a7c987b02508de8b10b67f869efac93
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-10-23 23:51:27 +0530

    DS assignment 1 readme added

---
+++ sem_3/data_structures/assignment_1/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findMajorityElem(int a[], int n)
+{
+   // Finding the maximum element in the array
+   int max = a[0];
+   for (int i = 0; i < n; i++)
+      if (a[i] >= max)
+         max = a[i];
+   // Using a new array as a hashmap using the original array elements as key(here index of array) and there number of occurrences as value
+   int *arr = (int *)calloc(max + 1, sizeof(int));
+
+   // Calculating number of occurrences of each element and storing it as key value pairs in arr
+   for (int i = 0; i < n; i++)
+      arr[a[i]]++;
+
+   // If there is an element whose occurrence is greater than half the length of the array then it is returned otherwise -1 is returned
+   for (int i = 0; i < n; i++)
+   {
+      if (arr[a[i]] > n / 2)
+      {
+         free(arr);
+         return a[i];
+      }
+   }
+
+   free(arr);
+   return -1;
+}
+
+int main()
+{
+   int a[8] = {2, 1, 2, 2, 6, 2, 2, 6};       // Input array
+   int n = sizeof(a) / sizeof(a[0]);          // Calculating length of array
+   int majorityElem = findMajorityElem(a, n); // Finding the majority element
+   majorityElem >= 0 ? printf("The majority element is: %d", majorityElem) : printf("There is no majority element");
+   // Priting the majority element if there is any otherwise printing There is no majority element
+   return 0;
+}
---
+++ sem_3/data_structures/assignment_1/readme.md
@@ -0,0 +1,23 @@
+### Statement
+
+Write a c code for the following:
+**Input:** a set of n numbers in an array A
+**Output:** If there exist a majority element, print it else print no majority element.
+
+> What is a Majority element? Majority element is one which occurs more than half of the total number of elements in the array, ie, if there are n elements, then more than floor (n/2) times.
+
+**Example 1:** Input: A = [ 2, 1, 2, 2, 6, 2, 2, 6]. then n = 8.
+Majority element is one which occurs more than floor (8/2) = 4 times in A.
+Output: 2, since it occurs 5 times (5>4) in A.
+
+**Example 2:** Input: A = [ 2, 1, 2, 2, 6, 1, 7, 6]. then n = 8.
+Majority element is one which occurs more than floor (8/2) = 4 times in A.
+Output: No majority element, Since none of 1,2,6,7 in A occurs more than 4 times in A.
+
+Upload the a) program (.c file) in classroom along with a b) documentation (.docx) that contains the following:
+
+1. Input, Output clearly defined
+2. Algorithm as statements or Pseudocode or flowchart (not the program)
+3. A dry run of your algorithm
+
+> Note: Please do not copy from internet/ friend. You will be penalized if you copy.
