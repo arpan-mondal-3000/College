1e9ae1607a9f32614f9985d5bf44338e2b7ec0f5
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2025-03-05 12:43:49 +0530

    process forking

---
+++ sem_4/os/process_forking/fig3-32.c
@@ -0,0 +1,24 @@
+/**
+ * Figure 3.32
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void main()
+{
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        fork();
+
+        // This printf statement is for debugging purposes
+        // getppid(): gets the parent process-id
+        // getpid(): get child process-id
+
+        printf("[%d] [%d] i=%d\n", getppid(), getpid(), i);
+    }
+
+    printf("[%d] [%d] hi\n", getppid(), getpid());
+}
