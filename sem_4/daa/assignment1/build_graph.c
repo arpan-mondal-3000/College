38116f2da0cf21d958adfd44530b4a28abed86c1
arpan-mondal-3000 <arpan954mondal@gmail.com>
2025-03-20 01:23:18 +0530

    build graph corrected

--- sem_4/daa/assignment1/build_graph.c
+++ sem_4/daa/assignment1/build_graph.c
@@ -11,7 +11,7 @@ int main()
     printf("Enter number of edges: ");
     scanf("%d", &m);
 
-    int total_edges = n * n;
+    int total_edges = (n * (n + 1)) / 2;
 
     if (m > total_edges)
     {
@@ -20,12 +20,22 @@ int main()
     }
 
     FILE *fp = fopen("graph.txt", "w");
+    int **adj_matrix = (int **)malloc(sizeof(int *) * n);
+    for (int i = 0; i < n; i++)
+        adj_matrix[i] = (int *)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
+            if (j < i)
+            {
+                adj_matrix[i][j] = adj_matrix[j][i];
+                fprintf(fp, "%d ", adj_matrix[i][j]);
+                continue;
+            }
             if (total_edges - (i * n + j) == m)
             {
+                adj_matrix[i][j] = 1;
                 fprintf(fp, "1 ");
                 m--;
                 continue;
@@ -33,14 +43,22 @@ int main()
             int edge = rand() % 2;
             if (m && edge)
             {
+                adj_matrix[i][j] = 1;
                 fprintf(fp, "1 ");
                 m--;
             }
             else
+            {
+                adj_matrix[i][j] = 0;
                 fprintf(fp, "0 ");
+            }
         }
         fprintf(fp, "\n");
     }
+
+    for (int i = 0; i < n; i++)
+        free(adj_matrix[i]);
+    free(adj_matrix);
 
     fclose(fp);
     return 0;
