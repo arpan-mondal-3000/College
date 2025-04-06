2d29a820a525a6f50f2d3de5bc753c5eab2a2907
Arpan Mondal <arpan954mondal@gmail.com>
2025-04-06 18:26:50 +0530

    DAA DFS assignment

--- sem_4/daa/assignment2/main.c
+++ sem_4/daa/assignment2/main.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int x;
+    int y;
+    struct node *link;
+} node;
+
+node *path = NULL;
+node *temp = NULL;
+int df = 1;
+
+void DFS(int **grid, int xs, int ys, int xd, int yd, int **mark, int n)
+{
+    // initialize path at source node
+    if (!path)
+    {
+        node *start = (node *)malloc(sizeof(node));
+        start->x = xs;
+        start->y = ys;
+        path = start;
+        temp = path;
+    }
+    else // add each node to the path
+    {
+        printf("Visiting: %d, %d\n", xs, ys);
+        node *next = (node *)malloc(sizeof(node));
+        next->x = xs;
+        next->y = ys;
+        temp->link = next;
+        temp = next;
+        mark[xs][ys] = 1;
+    }
+    // End if destinaiton reached
+    if (xs == xd && ys == yd)
+    {
+        temp->link = NULL;
+        printf("Destination reached: %d, %d\n", xs, ys);
+        mark[xs][ys] = 1;
+        df = 0;
+        return;
+    }
+
+    // check top
+    if (xs != 0 && grid[xs - 1][ys] == 0 && mark[xs - 1][ys] == 0 && df)
+    {
+        DFS(grid, xs - 1, ys, xd, yd, mark, n);
+    }
+    // check right
+    if (ys != n - 1 && grid[xs][ys + 1] == 0 && mark[xs][ys + 1] == 0 && df)
+    {
+        DFS(grid, xs, ys + 1, xd, yd, mark, n);
+    }
+    // check bottom
+    if (xs != n - 1 && grid[xs + 1][ys] == 0 && mark[xs + 1][ys] == 0 && df)
+    {
+        DFS(grid, xs + 1, ys, xd, yd, mark, n);
+    }
+    // check left
+    if (ys != 0 && grid[xs][ys - 1] == 0 && mark[xs][ys - 1] == 0 && df)
+    {
+        DFS(grid, xs, ys - 1, xd, yd, mark, n);
+    }
+}
+
 int main()
 {
     FILE *fp = fopen("grid.txt", "r");
@@ -13,10 +77,10 @@ int main()
     int n; // nxn grid
     fscanf(fp, "%d", &n);
     int xs, ys, xd, yd;
-    printf("Enter source coordinates(xs, ys): ");
+    printf("Enter source coordinates(xs, ys): "); // e.g : 0 6
     scanf("%d %d", &xs, &ys);
 
-    printf("Enter destination coordinates(xd, yd): ");
+    printf("Enter destination coordinates(xd, yd): "); // e.g : 10 18
     scanf("%d %d", &xd, &yd);
 
     // Allocating memory for the grid
@@ -25,15 +89,38 @@ int main()
     for (int i = 0; i < n; i++)
         grid[i] = (int *)malloc(n * sizeof(int));
 
+    // Allocating memory for mark array
+    int **mark; // 0: undiscovered; 1: discovered; 2: finished
+    mark = (int **)calloc(n, sizeof(int *));
+    for (int i = 0; i < n; i++)
+        mark[i] = (int *)calloc(n, sizeof(int));
+
     // Reading the grid from the file
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             fscanf(fp, "%d", &grid[i][j]);
 
+    DFS(grid, xs, ys, xd, yd, mark, n);
+
+    node *temp = path;
+    while (temp)
+    {
+        if (temp->link)
+            printf("(%d, %d) -> ", temp->x, temp->y);
+        else
+            printf("(%d, %d)\n", temp->x, temp->y);
+        temp = temp->link;
+    }
+
     // Deallocating memory of the grid
     for (int i = 0; i < n; i++)
         free(grid[i]);
     free(grid);
+
+    // Deallocating memory of the mark
+    for (int i = 0; i < n; i++)
+        free(mark[i]);
+    free(mark);
 
     fclose(fp);
     return 0;
